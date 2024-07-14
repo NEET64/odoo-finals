7e649165d8ca1b2f93282bc933ed430690b5b826
SahilEagle <skeaglecompany@gmail.com>
2024-07-14 11:55:00 +0530

    user/me route done

--- backend/router/index.js
+++ backend/router/index.js
@@ -2,10 +2,10 @@ const { Router } = require('express');
 const router = Router();
 
 // routes imported
-// const userRoutes = require('./user.js');
+const userRoutes = require('./user.js');
 // const booksRoutes = require("./book.js");
 
-// router.use('/user', userRoutes); // api/user/...
+router.use('/user', userRoutes); // api/user/...
 // router.use("/book", booksRoutes); // api/books/...
 
 module.exports = router;
