f3781dd2d4c9b5af975a36461b45abf5a49680cf
Neet <neetdhameliya321@gmail.com>
2024-07-14 14:30:18 +0530

    minor fix

--- backend/router/book.js
+++ backend/router/book.js
@@ -4,49 +4,66 @@ const Book = require("../model/book");
 const router = Router();
 
 router.post("/add", async (req, res) => {
-    try {
-        const { title, author, publisher, publishedYear, categories, quantities, description, image_url } = req.body;
+  try {
+    const {
+      title,
+      author,
+      publisher,
+      publishedYear,
+      categories,
+      quantities,
+      description,
+      image_url,
+    } = req.body;
 
-        const book = new Book({
-            title, author, publisher, categories, description, image_url, publishedYear, quantities
-        });
-        await book.save();
+    const book = new Book({
+      title,
+      author,
+      publisher,
+      categories,
+      description,
+      image_url,
+      publishedYear,
+      quantities,
+    });
+    await book.save();
 
-        res.status(200).json({ message: "book saved successfully" });
-
-    } catch (error) {
-        console.log(error);
-        res.status(200).json({ message: error });
-    }
-
-})
+    res.status(200).json({ message: "book saved successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(200).json({ message: error });
+  }
+});
 
 router.get(`/all`, async (req, res) => {
-    try {
-        const apiKey = process.env.API_KEY;
-        const url = `https://www.googleapis.com/books/v1/volumes?q=a&key=${apiKey}`;
+  try {
+    const apiKey = process.env.API_KEY;
+    const url = `https://www.googleapis.com/books/v1/volumes?q=a&key=${apiKey}`;
 
-        const response = await fetch(url);
-        const data = await response.json();
+    const response = await fetch(url);
+    const data = await response.json();
 
-        const books = data.items.map(item => {
-            return {
-                title: item.volumeInfo.title,
-                authors: item.volumeInfo.authors ? item.volumeInfo.authors.join(', ') : 'Unknown',
-                publisher: item.volumeInfo.publisher || 'Unknown',
-                publishedYear: item.volumeInfo.publishedDate,
-                categories: item.volumeInfo.categories,
-                description: item.volumeInfo.description,
-                image_url: item.volumeInfo.imageLinks?.thumbnail || item.volumeInfo.imageLinks?.smallThumbnail || '',
-            };
-        });
+    const books = data.items.map((item) => {
+      return {
+        title: item.volumeInfo.title,
+        authors: item.volumeInfo.authors
+          ? item.volumeInfo.authors.join(", ")
+          : "Unknown",
+        publisher: item.volumeInfo.publisher || "Unknown",
+        publishedYear: item.volumeInfo.publishedDate,
+        categories: item.volumeInfo.categories,
+        description: item.volumeInfo.description,
+        image_url:
+          item.volumeInfo.imageLinks?.thumbnail ||
+          item.volumeInfo.imageLinks?.smallThumbnail ||
+          "",
+      };
+    });
 
-        console.log(books); // Log the extracted book data
-
-        // res.status(200).json(data);
-    } catch (error) {
-        console.log(error);
-    }
+    return res.status(200).json(books);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
