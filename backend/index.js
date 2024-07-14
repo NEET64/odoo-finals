53032a61f8d26b6d43233dea22ba22493f98d52f
SahilEagle <skeaglecompany@gmail.com>
2024-07-14 10:23:55 +0530

    google authentication done

--- backend/index.js
+++ backend/index.js
@@ -4,21 +4,30 @@ const dotenv = require("dotenv");
 dotenv.config();
 const passport = require("passport");
 const session = require("express-session");
-const port = process.env.PORT || 3000;
+const cors = require("cors");
 
 const connectDB = require("./configuration/connection");
-
-const cors = require("cors");
+connectDB();
 
 const router = require("./router/index");
 
+require("./configuration/passport");
+
 // allow access of the api
-app.use(cors({ origin: process.env.FRONTEND }));
+app.use(cors({ origin: process.env.FRONTEND, credentials: true }));
 
 // to support incoming form data in json
 app.use(express.json());
 
-connectDB();
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 const path = require("path");
 app.use("/public", express.static(path.join(__dirname, "./public")));
@@ -32,17 +41,36 @@ app.get("/", (req, res) => {
     });
 });
 
-// managing errors
-app.use((err, req, res, next) => {
-    let { statusCode = 500, message = "Something went wrong" } = err;
-    res.status(statusCode).json({
-        message: message,
-    });
+app.get(
+    "/auth/google",
+    passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get("/auth/google/callback",
+    passport.authenticate('google', { failureRedirect: "/login", session: true }),
+    (req, res) => {
+        res.redirect(`${process.env.FRONTEND}/home`);
+    }
+);
+
+app.post('/signup', passport.authenticate('local-signup'), (req, res) => {
+    if (req.user) {
+        res.status(201).json({ message: 'User created successfully!' });
+    } else {
+        res.status(400).json({ message: 'Signup failed.' });
+    }
 });
 
-app.post("/login", )
+app.post('/login', passport.authenticate('local-login'), (req, res) => {
+    if (req.user) {
+        res.status(200).json({ message: 'Login successful!' });
+    } else {
+        res.status(401).json({ message: 'Login failed.' });
+    }
+});
 
 
+const port = process.env.PORT || 3000;
 
 // setting up http server at port
 app.listen(port, () => {
